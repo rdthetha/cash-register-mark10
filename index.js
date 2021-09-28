cd9b7e66cf920e63a484e3ff72171d6ebc539f7f
rdthetha <rd10072000@gmail.com>
2021-09-28 15:19:57 +0530

    added firstbutton feat

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const billAmt = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const firstmessage=document.querySelector("#first-error");
 const noteValue = document.querySelectorAll(".note-value");
+const checkFirstButton=document.querySelector("#first-button");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
+checkFirstButton.addEventListener("click",function firstValidity(){
+hideMessage();
+if (billAmt.value > 0) {}
+else{
+    firstshowMessage("Bill amount cannot be negative");
+}
+})
 checkButton.addEventListener("click", function validateAmount() {
     hideMessage();
 
@@ -25,6 +34,7 @@ checkButton.addEventListener("click", function validateAmount() {
 
 function hideMessage() {
     message.style.display = "none";
+    firstmessage.style.display="none";
 }
 
 function showMessage(m) {
@@ -32,7 +42,11 @@ function showMessage(m) {
     message.style.display = "block";
     message.innerText = m;
 }
+function firstshowMessage(m) {
 
+    firstmessage.style.display = "block";
+    firstmessage.innerText = m;
+}
 function calculateChange(change) {
     for (let i = 0; i < availableNotes.length; i++) {
         const noOfNotes = Math.trunc(change / availableNotes[i]);
