f432ce68f8b4bfe3600644752607783f1c6424c7
rdthetha <rd10072000@gmail.com>
2021-09-28 14:01:55 +0530

    simple setup

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-const billAmt=document.querySelector("#bill-amount");
-const cashGiven=document.querySelector("#cash-given");
-const checkButton=document.querySelector("#check-button");
-const message=document.querySelector("#error-message");
+const billAmt = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noteValue = document.querySelectorAll(".note-value");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
+checkButton.addEventListener("click", function validateAmount() {
+    hideMessage();
 
-checkButton.addEventListener("click",function validateAmount(){
-    
-    
-    if(billAmt.value>0)
-    {
-     
-    }
-    else{
-        message.innerText="Invaliddd";
+    if (billAmt.value > 0) {
+        if (cashGiven.value >= billAmt.value) {
+            const change = cashGiven.value - billAmt.value;
+            calculateChange(change);
+        } else {
+            showMessage("Cash Given should be more than or equal to bill amount");
+        }
+
+    } else {
+        showMessage("Bill amount cannot be negative");
     }
 
 });
 
 
-function showMessage(message){
-    
-    message.innerText="Invaliddd";
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(m) {
+
+    message.style.display = "block";
+    message.innerText = m;
+}
+
+function calculateChange(change) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const noOfNotes = Math.trunc(change / availableNotes[i]);
+        change = change % availableNotes[i];
+        noteValue[i].innerText = noOfNotes;
+    }
+
+
 }
